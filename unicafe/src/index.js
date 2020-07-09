98c7b152da792e0070945d8471a3823e24eb84f3
Denzel Perez <denzelperez@denzels-mbp.attlocal.net>
2020-07-08 19:13:43 -0700

    finished ancedotes assignments

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,17 +1,113 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Button = ({onClick, text}) => {
+  return(
+    <button onClick = {onClick} >{text}</button>
+  )
+ 
+}
+
+const Average = (props) => {
+  if(props.total === 0){
+    return(
+      <table>
+      <tbody>
+      <tr>{props.text}0</tr>
+      </tbody>      
+    </table>  
+    )
+  }
+  return(
+    <table>
+      <tbody>
+      <tr>{props.text}{(props.good+props.bad)/props.total}</tr>
+      </tbody>      
+    </table>
+  )
+}
+
+const PostivePercent = (props) => {
+  if(props.total === 0){
+    return(
+      <table>
+      <tbody>
+      <tr>{props.text}0</tr>
+      </tbody>      
+    </table>  
+    )
+  }
+  return(
+    <table>
+      <tbody>
+      <tr>{props.text}{(props.good/props.total)*100}%</tr>
+      </tbody>   
+    </table>
+    
+  )
+}
+
+const Statistics = (props) => {
+  if(props.text ==='Average:'){
+    return(   
+        <Average good={props.good} bad={props.bad} total={props.total} text={props.text} />
+    )
+  }
+
+  if(props.text ==='Positive:'){
+    return(
+          <PostivePercent total={props.total} good={props.good} text={props.text} />
+    )
+  }
+
+    return(
+      
+        <table>
+        <tbody>
+        <tr>
+        <td>{props.text} {props.value} </td>
+        </tr>        
+        </tbody>
+        </table>
+    
+    )
+  }
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodButton = () => {
+    setGood(good + 1);
+  }
+
+  const handleNeutralButton = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const handleBadButton = () => {
+    setBad(bad+1);
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button onClick={handleGoodButton} text = 'good' />
+      <Button onClick={handleNeutralButton} text = 'neutral' />
+      <Button onClick={handleBadButton} text = 'bad' />
+      <h3>Statistics</h3>
+      <Statistics text='Good:' value={good} />
+      <Statistics text='Neutral:' value={neutral} />
+      <Statistics text='Bad:' value={bad} />
+      <Statistics text='Total:' value={good+bad+neutral} />
+      <Statistics text='Average:' good={good} bad={bad *-1} total={good+bad+neutral} />
+      <Statistics text='Positive:' total={good+bad+neutral} good={good} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
